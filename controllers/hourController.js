ca4b09294c7412653d9fa97539dca91cd07d98a2
Matheus Carvalheira <matheuscarvalheiraf@gmail.com>
2024-06-18 19:42:09 -0300

    sharing responsibilities with the controller

--- controllers/hourController.js
+++ controllers/hourController.js
@@ -2,49 +2,49 @@ const Hour = require("../model/Hour");
 const db = require("../config/database");
 
 const hourController = {
-    newHour: (req, res) => {
-        const query = "SELECT * FROM hours";
+  newHour: (req, res) => {
+    const query = "SELECT * FROM hours";
 
-        db.all(query, (err, hours) => {
-            if (err) {
-                console.error("Erro ao buscar horas:", err.message);
-                res.status(500).send("Erro ao buscar horas");
-                return;
-            }
-            res.render("hours/newHour", { title: "Nova Hora", hours: hours });
-        });
-    },
+    db.all(query, (err, hours) => {
+      if (err) {
+        console.error("Erro ao buscar horas:", err.message);
+        res.status(500).send("Erro ao buscar horas");
+        return;
+      }
+      res.render("hours/newHour", { title: "Nova Hora", hours: hours });
+    });
+  },
 
-    listHours: (req, res) => {
-        Hour.findAll((err, hours) => {
-            if (err) {
-                return res.status(500).json({ message: "Erro ao listar horas" });
-            }
-            res.render("hours/list", { title: "Lista de Horas", hours });
-        });
-    },
+  listHours: (req, res) => {
+    Hour.findAll((err, hours) => {
+      if (err) {
+        return res.status(500).json({ message: "Erro ao listar horas" });
+      }
+      res.render("hours/list", { title: "Lista de Horas", hours });
+    });
+  },
 
-    hoursByUser: (req, res) => {
-        const userId = parseInt(req.params.userId);
-        Hour.findByUserId(userId, (err, hours) => {
-            if (err) {
-                return res.status(500).json({ message: "Erro ao buscar horas" });
-            }
-            res.render("hours/list", { title: "Horas do Usuário", hours });
-        });
-    },
+  hoursByUser: (req, res) => {
+    const userId = parseInt(req.params.userId);
+    Hour.findByUserId(userId, (err, hours) => {
+      if (err) {
+        return res.status(500).json({ message: "Erro ao buscar horas" });
+      }
+      res.render("hours/list", { title: "Horas do Usuário", hours });
+    });
+  },
 
-    createHour: (req, res) => {
-        const { userId, date, startTime, endTime } = req.body;
+  createHour: (req, res) => {
+    const { userId, date, startTime, endTime } = req.body;
 
-        Hour.create(userId, date, startTime, endTime, (err, newHour) => {
-            if (err) {
-                return res.status(500).json({ message: "Erro ao registrar hora" });
-            }
+    Hour.create(userId, date, startTime, endTime, (err, newHour) => {
+      if (err) {
+        return res.status(500).json({ message: "Erro ao registrar hora" });
+      }
 
-            res.redirect(req.headers.referer || "/");
-        });
-    }
+      res.redirect(req.headers.referer || "/");
+    });
+  },
 };
 
 module.exports = hourController;
